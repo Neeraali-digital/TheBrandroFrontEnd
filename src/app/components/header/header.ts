e87fb92d7335a540c4cef08334fb54692855b070
Neeraali-digital <neeraalidigitalweb@gmail.com>
2025-07-24 15:11:47 +0530

    .

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.html'
 })
 export class Header {
+  isMobileMenuOpen = false;
 
+  navigationItems = [
+    { label: 'Home', href: '#home' },
+    { label: 'Services', href: '#services' },
+    { label: 'Portfolio', href: '#portfolio' },
+    { label: 'Contact', href: '#contact' }
+  ];
+
+  toggleMobileMenu() {
+    this.isMobileMenuOpen = !this.isMobileMenuOpen;
+  }
+
+  closeMobileMenu() {
+    this.isMobileMenuOpen = false;
+  }
 }
